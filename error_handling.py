812e582007d809cd87c770b041786ce277a83d9d
tyharbert <tyharbert@users.noreply.github.com>
2024-07-16 12:08:28 -0400

    moving error handler back for the sake of the unit test

--- error_handling.py
+++
@@ -1,8 +0,0 @@
-from flask import jsonify
-from api import app
-
-@app.errorhandler(Exception)
-def handle_exception(exception: Exception):
-    # You can log the exception here using your preferred logging method
-    print(f"'{type(exception).__name__}: {exception}'")
-    return jsonify({"success": False, "error": "Internal Server Error"}), 500
